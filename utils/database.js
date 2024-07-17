ce1e59e588704eca4ec0286ff881fd005d9d5045
Nazar Palamarchuk <UA.Nazar.Palamarchuk.UA@gmail.com>
2024-07-17 23:53:57 +0300

    chore: Remove unnecessary options for MongoDB connection

--- utils/database.js
+++ utils/database.js
@@ -13,14 +13,14 @@ export const connectToDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI, {
 			dbName: "share_prompt",
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
+			// useNewUrlParser: true,
+			// useUnifiedTopology: true, // The useNewUrlParser and useUnifiedTopology options are no longer necessary
 		})
 
 		isConnected = true
 
 		console.log("MongoDB connected")
 	} catch (error) {
-		console.log(error)
+		console.log("MongoDB connection error:", error)
 	}
 }
